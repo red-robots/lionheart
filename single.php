c59ffc175e11d31a4af2485328f3e5e8cf0e4785
lisadebona <annalisa.quiamco@gmail.com>
2019-02-22 23:35:12 +0800

    Updated single post page

--- single.php
+++ single.php
@@ -1,35 +1,21 @@
 <?php
-/**
- * The template for displaying all single posts.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package ACStarter
- */
-
+$banner_image = get_field('subpage_banner');
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="content-area-sidebar single-page-content clear <?php echo ($banner_image) ? 'has-banner':'no-banner';?>">
+			<main id="main" class="site-main wrapper content-inner" role="main">
+				<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php
-		while ( have_posts() ) : the_post();
+					<?php if($banner_image) { ?>
+						<?php the_content(); ?>
+					<?php } else { ?>
+						<?php get_template_part( 'template-parts/content', 'page' ); ?>
+					<?php } ?>
 
-			get_template_part( 'template-parts/content', get_post_format() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
+				<?php endwhile;  ?>
+			</main><!-- #main -->
+		</div>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
